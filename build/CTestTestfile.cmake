31b55f34757a9b8786f2f081fc655c469f0a2a54
MIchael <michael.h.fitzgerald@case.edu>
2021-10-09 13:41:35 -0400

    mySecondCommit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/michael/lab2/catkin_ws/src
+# Build directory: /home/michael/lab2/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
